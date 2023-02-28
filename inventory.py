e5afa62fbf352763ec77d6dfcf6f6438a3a185a5
Scarlett-the-Gray <125994490+Scarlett-the-Gray@users.noreply.github.com>
2023-02-28 13:10:30 +0200

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -3,156 +3,112 @@
 from tabulate import tabulate
 
 class Shoes:
-#create a class named Shoes with the following attributes:
-#country, code, product, cost, quantity
-
-    def __init__(self, country, code, product, cost, quantity):# self -  access attributes and methods of the class in python
+    def __init__(self, country, code, product, cost, quantity):
         self.country = country
         self.code = code
         self.product = product
         self.cost = cost
         self.quantity = quantity
 
-#________________________________________________________________________________________
-# define functions inside the class
-#get_cost, get_quantity, get_country,  get_code ,get_product, set_quantity, __str__
-
     def get_cost(self):
         return self.cost
+
     def get_quantity(self):
         return self.quantity
+
     def get_country(self):
         return self.country
+
     def get_code(self):
-        return self.code  
+        return self.code
+
     def get_product(self):
         return self.product
+
     def set_quantity(self, new_quant):
         self.quantity = new_quant
 
     def __str__(self):
         return f"{self.country},{self.code},{self.product},{self.cost},{self.quantity}\n".upper()
 
-#_________________________________________________________________________________________
-inventory_read = open("inventory.txt", "r")#open txt doc
-inventory_write = open("inventory.txt", "a+")#write to txt doc - new input
+with open("inventory.txt", "r") as inventory_read, open("inventory.txt", "a+") as inventory_write:
+    items_list = []
+    shoe_obj = []
 
-items_list = []# create a variables with an empty list
-shoe_obj = []
+    def read_shoes_data():
+        try:
+            for lines in inventory_read:
+                strip_lines = lines.strip("\n")
+                split_lines = strip_lines.split(",")
+                items_list.append(split_lines)
 
-#_________________________________________________________________________________________
-#define functions outside the class
-#read_shoes_data
-#capture_shoes
-#view_all
-#re_stock
-#search_shoe
-#value of shoes
-#overstocked-shoes for sale
+            for i in range(1, len(items_list)):
+                array = items_list[i]
+                shoe1 = Shoes(array[0], array[1], array[2], array[3], int(array[4]))
+                shoe_obj.append(shoe1)
 
-#_____________________________#read_shoes_data
-def read_shoes_data():
-    file = None
-    try:
-        for lines in inventory_read:
-            strip_lines = lines.strip("\n")
-            split_lines = strip_lines.split(",")
-            items_list.append(split_lines)
+        except FileNotFoundError as error:
+            print("\n       Error message: File not found\n")
+            print(error)
 
-        for i in range(1, len(items_list)):
-            array = items_list[i]
-            shoe1 =  Shoes(array[0], array[1], array[2], array[3], int(array[4]))
-            shoe_obj.append(shoe1)
-            
-    except FileNotFoundError as error:
-        print("\n       Error messagae:  File not found\n")
-        print(error)
+    def capture_shoes():
+        try:
+            new_country = input("Enter country name: \n")
+            new_code = input("Enter code of product: \n")
+            new_product = input("Enter name of product:\n")
+            new_cost = int(input("Enter cost of product.  Do not include currency code.: \n"))
+            new_quantity = int(input("Enter quantity of product received.:\n"))
 
-    finally:
-        if file is not None:
-            file.close()
+            new_shoe = Shoes(new_country, new_code, new_product, new_cost, new_quantity)
+            shoe_obj.append(new_shoe)
 
-#_______________________________#capture_shoes
-#input of new stock
-#info needed: country,code,product,name,cost and quantity
-#acknowledge captured stock
-def capture_shoes():  
-    file = None
-    try:
-        new_country = input("Enter country name: \n")
-        new_code = input("Enter code of product: \n")
-        new_product = input("Enter name of product:\n")
-        new_cost = int(input("Enter cost of product.  Do not include currency code.: \n"))
-        new_quantity = int(input("Enter quantity of product received.:\n"))
+            inventory_write.write(f'\n{new_country},{new_code},{new_product},{new_cost},{new_quantity}')
+            print("\n       New stock captured successfully.\n")
 
-        new_shoe = Shoes(new_country, new_code, new_product, new_cost, new_quantity)
-        shoe_obj.append(new_shoe)
+        except FileNotFoundError as error:
+            print("\n       Error message: File not found\n")
+            print(error)
 
-        inventory_write.write(f'\n{new_country},{new_code},{new_product},{new_cost},{new_quantity}')
-        print("\n       New stock captured successfully.\n")
-        inventory_write.close()
+    def view_all():
+        try:
+            print("\n                        Inventory Control Sheet\n")
+            country = []
+            code = []
+            product = []
+            cost = []
+            table = []
+            quantity = []
 
-    except FileNotFoundError as error:
-        print("\n       Error messagae:  File not found\n")
-        print(error)
+            for lines in shoe_obj:
+                country.append(lines.get_country())
+                code.append(lines.get_code())
+                product.append(lines.get_product())
+                cost.append(lines.get_cost())
+                quantity.append(lines.get_quantity())
+            table = zip(country, code, product, cost, quantity)
+            print(tabulate(table, headers=('Country', 'Code', 'Product', 'Cost', 'Quantity'), tablefmt='fancy_grid'))
 
-    finally:
-        if file is not None:
-            file.close()
+        except FileNotFoundError as error:
+            print("\n       Error message: File not found\n")
+            print(error)
 
-#__________________________________#view_all
-def view_all():
-
-    file = None
-    
-    try:
-        print("\n                        Inventory Control Sheet\n")
+    def restock():
+        restock_list = []
         country = []
         code = []
         product = []
         cost = []
-        table  = []
         quantity = []
+        table = []
 
-        for lines in shoe_obj:
-            country.append(lines.get_country())
-            code.append(lines.get_code())
-            product.append(lines.get_product())
-            cost.append(lines.get_cost())
-            quantity.append(lines.get_quantity())
-        table = zip(country, code, product, cost, quantity)
-        print(tabulate(table, headers = ('Country','Code', 'Product', 'Cost', 'Quantity'), tablefmt='fancy_grid'))
- 
-    except FileNotFoundError as error:
-        print("\n       Error messagae:  File not found\n")
-        print(error)
+        try:
+            shoe_obj.sort(key=lambda x:x.quantity)
 
-    finally:
-        if file is not None:
-            file.close()
-
-#___________________________________#re_stock
-#indicate low level stock 
-#restock
-def restock():
-
-    file = None
-
-    restock_list = []
-    country = []
-    code = []
-    product = []
-    cost = []
-    quantity = []
-    table  = []
-
-    try:
-        shoe_obj.sort(key=lambda x:x.quantity)
-
-        for i in range(1,6):
-            restock_list.append(shoe_obj[i])
+            for i in range(1,6):
+                restock_list.append(shoe_obj[i])
     
-        print("\n                Choose product to restock from list below:\n")
+            print("\n                Choose product to restock from list below:\n")
+
 
         for line in restock_list:
             country.append(line.get_country())
